a89c70fae65d921bae49bdaba650813d8eab6df8
nmarth2993 <nmarth2993@gmail.com>
2023-03-15 19:27:17 -0400

    Update backend to handle deletion and basic data validation

--- UserAPI/cmsc_site.py
+++ UserAPI/cmsc_site.py
@@ -21,7 +21,7 @@ class UserModel(db.Model):
         self.points = points
 
     def __repr__(self) -> str:
-        return f"{self.lastname}, {self.firstname}:{self.person_id}"
+        return f"{self.name}, {self.person_id}:{self.points}"
 
 
 app = Flask(__name__)
@@ -44,6 +44,17 @@ def hello_world():
     return json.dumps({"suggestion": a})
 
 
+@app.route('/search', methods=['GET'])
+def search():
+    name = request.args.get('user')
+
+    person = db.session.query(UserModel).filter_by(name=name).first()
+    print(f"person: {person}")
+    if person is not None:
+        return json.dumps({'success': True, 'status': 200, 'name': person.name, 'id': person.person_id, 'points': person.points})
+    return json.dumps({'success': False, 'status': 404})
+
+
 @app.route('/create', methods=['POST', 'PUT'])
 def create():
     # data = request.get_data().decode()
@@ -53,6 +64,9 @@ def create():
     name = data.get('name')
     points = data.get('points')
 
+    if person_id is None or name is None or points is None:
+        return json.dumps({'success': False, 'status': 400})
+
     person = UserModel(person_id, name, points)
 
     db.session.add(person)
@@ -60,17 +74,28 @@ def create():
     return json.dumps({'success': True, 'status': 200})
 
 
-@app.route('/delete', methods=['POST'])
+@app.route('/delete', methods=['DELETE'])
 def delete():
     data = request.get_json()
     person_id = data.get('person_id')
     name = data.get('name')
     points = data.get('points')
-    person = UserModel(person_id, name, points)
 
-    db.session.remove(person)
-    db.session.commit()
-    return json.dumps({'success': True, 'status': 200})
+    if name is None:
+        return json.dumps({'success': False, 'status': 400})
+    # person = UserModel(person_id, name, points)
+
+    # query the database
+    # person = db.session.query(UserModel).filter(UserModel.name == name).filter(
+    # UserModel.person_id == person_id).filter(UserModel.points == points)
+    person = db.session.query(UserModel).filter_by(name=name).first()
+    print(f"found db object {person}")
+
+    if person is not None:
+        db.session.delete(person)
+        db.session.commit()
+        return json.dumps({'success': True, 'status': 200})
+    return json.dumps({'success': False, 'status': 404})
 
 
 if __name__ == "__main__":
